e6fd35172c6c1daf6fce1170a868a681776fd91d
SRCadavieco <sergiorc2694@gmail.com>
2025-07-31 14:00:16 +0200

    Instalacion de breeze + tabla de links en base de datos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,25 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
+/**
+ * Register any application services.
+*/
+public function register(): void
+{
+    //
+}
+
+/**
+ * Bootstrap any application services.
+*/
+public function boot(): void
+{
+    // Set the default string length for database schema to avoid issues with older MySQL versions
+    Schema::defaultStringLength(191);
     }
 }
