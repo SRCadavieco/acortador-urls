2a9cf10dbf2ad4c98a09a31dd9532734142a6997
SRCadavieco <sergiorc2694@gmail.com>
2025-07-31 14:17:15 +0200

    Reajuste de nombres de columnas en la tabla y modelo de link

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -4,7 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+
 class Link extends Model
 {
-    //
+
+
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'original_url',
+        'shortened_url',
+        'custom_alias',
+        'password',
+        'expires_at',
+    ];
+
+    // Relación con usuario (opcional)
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
+
+
--- database/migrations/2025_07_31_115355_create_links_table.php
+++ database/migrations/2025_07_31_115355_create_links_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
 
 return new class extends Migration
 {
@@ -13,8 +14,8 @@ return new class extends Migration
     {
         Schema::create('links', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('original_url');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade')->nullable();
+            $table->text('original_url');
             $table->string('shortened_url')->unique();
             $table->string('custom_alias')->nullable()->unique();
         $table->string('password')->nullable();
