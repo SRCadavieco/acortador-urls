61b4fb0fb0a70ddde2fc2e2600f6607a4f518637
SRCadavieco <sergiorc2694@gmail.com>
2025-07-31 14:28:28 +0200

    Solucion error rutas de perfil en navigation y web, (Se cambiará en el futuro

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ Route::get('/{code}', function ($code) {
 // Dashboard solo para usuarios autenticados
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [LinkController::class, 'index'])->name('dashboard');
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
