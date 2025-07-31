9fa6595d6f53c53928b7f3b80815c1a91854ebaa
SRCadavieco <sergiorc2694@gmail.com>
2025-07-31 14:17:42 +0200

    Rutas y vista

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,27 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\LinkController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::post('/shorten', [LinkController::class, 'store'])->name('links.store');
 
+// Redirección pública
+Route::get('/{code}', function ($code) {
+    $link = \App\Models\Link::where('shortened_url', $code)
+                ->orWhere('custom_alias', $code)
+                ->firstOrFail();
+
+    return redirect($link->original_url);
+});
+
+// Dashboard solo para usuarios autenticados
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dashboard', [LinkController::class, 'index'])->name('dashboard');
 });
 
 require __DIR__.'/auth.php';
