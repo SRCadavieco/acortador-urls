8ec75806acbce51365ba121e3a29ed1bd07a5528
SRCadavieco <sergiorc2694@gmail.com>
2025-08-08 12:23:43 +0200

    Css del navegador

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,91 +1,71 @@
-<nav x-data="{ open: false }">
-  <div>
-    <div>
-      <div>
-        <!-- Logo -->
-        <div>
-          <a href="{{ url('/') }}">
-            <x-application-logo />
-          </a>
-        </div>
-
-      <!-- Menú derecho -->
-      <div>
-        @guest
-          <a href="{{ route('login') }}">Iniciar sesión</a>
-          <a href="{{ route('register') }}">Registrarse</a>
-        @else
-          <x-dropdown align="right" width="48">
-            <x-slot name="trigger">
-              <button>
-                <div>{{ Auth::user()->name }}</div>
-                <div>
-                  <svg viewBox="0 0 20 20"><path fill-rule="evenodd" clip-rule="evenodd"
-                     d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0
-                     111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"/>
-                  </svg>
-                </div>
-              </button>
-            </x-slot>
-
-            <x-slot name="content">
-              <x-dropdown-link :href="route('links.index')">
-                {{ __('Mis links') }}
-              </x-dropdown-link>
-              <x-dropdown-link :href="route('profile.edit')">
-                {{ __('Mi perfil') }}
-              </x-dropdown-link>
-              <!-- Logout -->
-              <form method="POST" action="{{ route('logout') }}">
-                @csrf
-                <x-dropdown-link :href="route('logout')"
-                                 onclick="event.preventDefault(); this.closest('form').submit();">
-                  {{ __('Cerrar sesión') }}
-                </x-dropdown-link>
-              </form>
-            </x-slot>
-          </x-dropdown>
-        @endguest
-      </div>
-
-      <!-- Menú hamburguesa responsive -->
-      <div>
-        <button @click="open = !open">
-          <svg stroke="currentColor" fill="none" viewBox="0 0 24 24">
-            <path :class="{'hidden': open, 'inline-flex': !open}" class="inline-flex"
-                  stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                  d="M4 6h16M4 12h16M4 18h16"/>
-            <path :class="{'hidden': !open, 'inline-flex': open}" class="hidden"
-                  stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                  d="M6 18L18 6M6 6l12 12"/>
-          </svg>
-        </button>
-      </div>
+<nav class="navbar" x-data="{ open: false }">
+  <div class="navbar-container">
+    <!-- Logo / Inicio -->
+    <div class="navbar-logo">
+      <a href="{{ url('/') }}" class="home-link">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon-home" viewBox="0 0 24 24" fill="none" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M3 12l9-9 9 9M4 10v10a1 1 0 001 1h3m10-11v10a1 1 0 01-1 1h-3m-4 0h4" />
+        </svg>
+        <span>Inicio</span>
+      </a>
     </div>
+
+    <!-- Botones de sesión -->
+    <div class="navbar-links">
+      @guest
+        <a href="{{ route('login') }}" class="btn-link">Iniciar sesión</a>
+        <a href="{{ route('register') }}" class="btn-link">Registrarse</a>
+      @else
+        <div class="dropdown">
+          <button class="dropdown-toggle">
+            <span>{{ Auth::user()->name }}</span>
+            <svg class="icon-chevron" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0
+                    111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"/>
+            </svg>
+          </button>
+          <div class="dropdown-menu">
+            <a href="{{ route('links.index') }}">Mis links</a>
+            <a href="{{ route('profile.edit') }}">Mi perfil</a>
+            <form method="POST" action="{{ route('logout') }}">
+              @csrf
+              <a href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();">
+                Cerrar sesión
+              </a>
+            </form>
+          </div>
+        </div>
+      @endguest
+    </div>
+
+    <!-- Botón hamburguesa -->
+    <button class="navbar-toggle" @click="open = !open">
+      <svg class="icon-menu" viewBox="0 0 24 24">
+        <path x-show="!open" d="M4 6h16M4 12h16M4 18h16" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path x-show="open" d="M6 18L18 6M6 6l12 12" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+    </button>
   </div>
 
   <!-- Menú responsive -->
-  <div :class="{'block': open, 'hidden': !open}">
-    <div>
-      <div>
-        <div>{{ Auth::check() ? Auth::user()->name : 'Invitado' }}</div>
-      </div>
-      <div>
-        @guest
-          <x-responsive-nav-link :href="route('login')">{{ __('Iniciar sesión') }}</x-responsive-nav-link>
-          <x-responsive-nav-link :href="route('register')">{{ __('Registrarse') }}</x-responsive-nav-link>
-        @else
-          <x-responsive-nav-link :href="route('profile.edit')">{{ __('Profile') }}</x-responsive-nav-link>
-          <x-responsive-nav-link :href="route('links.index')">{{ __('Mis links') }}</x-responsive-nav-link>
-          <form method="POST" action="{{ route('logout') }}">
-            @csrf
-            <x-responsive-nav-link :href="route('logout')"
-                onclick="event.preventDefault(); this.closest('form').submit();">
-              {{ __('Cerrar sesión') }}
-            </x-responsive-nav-link>
-          </form>
-        @endguest
-      </div>
+  <div class="navbar-responsive" :class="{ 'open': open }">
+    <div class="responsive-username">{{ Auth::check() ? Auth::user()->name : 'Invitado' }}</div>
+    <div class="responsive-links">
+      @guest
+        <a href="{{ route('login') }}">Iniciar sesión</a>
+        <a href="{{ route('register') }}">Registrarse</a>
+      @else
+        <a href="{{ route('links.index') }}">Mis links</a>
+        <a href="{{ route('profile.edit') }}">Mi perfil</a>
+        <form method="POST" action="{{ route('logout') }}">
+          @csrf
+          <a href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();">
+            Cerrar sesión
+          </a>
+        </form>
+      @endguest
     </div>
   </div>
 </nav>
